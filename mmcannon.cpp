1d01509bf906322cfa351ad670ceaca1b257b1ae
Bert <bbashfor@vols.utk.edu>
2018-11-24 18:46:11 -0500

    Initial Commit

---
+++ mmcannon.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <mpi.h>
+
+using namespace std;
+
+int main(){
+  float m1[128][128];
+  float m2[128][128];
+  
+  
+
+}
---
+++ mmserial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  float m1[128][128];
+  float m2[128][128];
+
+  //Popuate the matrixes with random numbers between 0 and 1.
+
+  //Matrix multiply
+
+  
+}
