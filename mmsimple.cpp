c287b8862f857748a9ad81e84a177a24a964f6b5
Bert <bbashfor@vols.utk.edu>
2018-11-26 21:23:24 -0500

    Yeet.

--- mmsimple.cpp
+++ mmsimple.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <cmath>
 #include <mpi.h>
+#include <ctime>
+#include <cstdllib>
 
 using namespace std;
 
 int main(){
-//  float m1[128*128];
-//  float m2[128*128];
-//  float a[128*128];
   int size, rank;
   double time;
   
@@ -18,45 +17,49 @@ int main(){
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	int row_color = rank / sqrt(size);
-	int col_color = rank % sqrt(size);
-
-	int num_row, num_col, i;
-	num_col = num_row = (128 / sqrt(size));
-
-	MPI_Comm row_comm;
-	MPI_Comm col_comm;
-
-	float **A; // Entire row beginning to end
-	float **B; // Entire column beginning to end
-	float **C; // Subsection result
-
-	MPI_Comm_split(MPI_COMM_WORLD, row_comm, rank, &row_comm);
-	MPI_Comm_split(MPI_COMM_WORLD, col_comm, rank, &col_comm);
-	
-
+  int row_color = rank / sqrt(size);
+  int col_color = rank % sqrt(size);
+  
+  int num_row, num_col, i, j;
+  num_col = num_row = (128 / sqrt(size));
+  srand(time(NULL)+rank);
+  
+  MPI_Comm row_comm;
+  MPI_Comm col_comm;
+  
+  float **A; // Entire row beginning to end
+  float **B; // Entire column beginning to end
+  float **C; // Subsection result
+  
+  MPI_Comm_split(MPI_COMM_WORLD, row_comm, rank, &row_comm);
+  MPI_Comm_split(MPI_COMM_WORLD, col_comm, rank, &col_comm);
+  
+  
   //Generate Matricies
-  	A = (float**) malloc(sizeof(float*) * num_row);
-  	for(i = 0; i < num_row; i++) {
-		A[i] = (float*) malloc(sizeof(float) * 128);
-  	}
-  	B = (float**) malloc(sizeof(float*) * 128);
-  	for(i = 0; i < num_row; i++) {
-  		B[i] = (float*) malloc(sizeof(float) * num_col);
-  	}
-  	C = (float**) malloc(sizeof(float*) * num_row);
-  	for(i = 0; i < num_row; i++) {
-		C[i] = (float*) malloc(sizeof(float) * num_col);
-	}
-/*  for(int i = 0; i < 128; i++){
-    m1[i*128+i] = 1;
-    m2[i*128+i] = 1;
+  A = (float**) malloc(sizeof(float*) * num_row);
+  for(i = 0; i < num_row; i++) {
+    A[i] = (float*) malloc(sizeof(float) * 128);
+    for(j = 0; j < 128; j++)
+      A[i][j] = (float) (rand/ (float) RAND_MAX);
   }
-*/  
-  if(rank == 0)
-      cout << "Matricies Set" << endl;  
-  //Algorithm
+  B = (float**) malloc(sizeof(float*) * 128);
+  for(i = 0; i < num_row; i++) {
+    B[i] = (float*) malloc(sizeof(float) * num_col);
+    for(j = 0; j < num_col; j++)
+      B[i][j] = (float) (rand/ (float) RAND_MAX);
+  }
+  C = (float**) malloc(sizeof(float*) * num_row);
+  for(i = 0; i < num_row; i++) {
+    C[i] = (float*) malloc(sizeof(float) * num_col);
+  }
 
+  if(rank == 0)
+    cout << "Matricies Set" << endl;
+  
+  //Communicate
+  
+  //Crunch Numbers  
+  
   
   if(rank == 0)
     cout << "Calculated" << endl;
