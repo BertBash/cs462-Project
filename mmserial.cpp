41baf0a84c0ab4a6d0c75ea39a8fceb67cbe2266
Joel Kovalcson <jkovalcs@acf-login2.acf.nics.utk.edu>
2018-11-28 16:09:52 -0500

    Uniform matrix generation

--- mmserial.cpp
+++ mmserial.cpp
@@ -1,40 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
 
 using namespace std;
 
 int main(){
-  float m1[128][128];
-  float m2[128][128];
-  float a[128][128];
-  
+  float A[128][128];
+  float B[128][128];
+  float C[128][128];
+  srand(-1337); 
   //Popuate the matrixes with random numbers between 0 and 1.
   for(int i = 0; i < 128; i++){
     for(int j = 0; j <128; j++){
-      m1[i][j] = 0;
-      m2[i][j] = 0;
-      a[i][j] = 0;
+      A[i][j] = (rand() / ((float) RAND_MAX / 2)) - 1;
+      B[i][j] = (rand() / ((float) RAND_MAX / 2)) - 1;
+      C[i][j] = 0;
     }
-    m1[i][i] = 1;
-    m2[i][i] = 1;
   }
   
   //Matrix multiply
   for(int i = 0; i < 128; i++){
-      for(int j = 0; j < 128; j++){
-	for(int k = 0; k < 128; k++){
-	  a[i][j] += m1[i][k] * m2[k][j];
-	}
+    for(int j = 0; j < 128; j++){
+      for(int k = 0; k < 128; k++){
+        C[i][j] += A[i][k] * B[k][j];
       }
+    }
   }
-  
-  //Test
-  for(int i = 0; i < 128; i++)
-    for(int j = 0; j < 128; j++)
-      if( i == j && a[i][j] != 1)
-	cout << "Failure." << a[i][j] << endl;
-      else if(i != j && a[i][j] != 0)
-	cout << "No way." << a[i][j] << endl;
 
+  ofstream output;
+
+  output.open("serial.output");
+  // Print the output to a file for testing
+  for(int i = 0; i < 128; i++) {
+  	  for(int j = 0; j < 128; j++) {
+  	  	   output << C[i][j];
+  	  }
+  	  output << endl;
+  }
   
   return 0;
 }
